1c55f3ceb86c5a5d8fdad3ecb4d36e57f9a1eeb9
Masataka Miyoshi <masataka.ehime.univ@gmail.com>
2018-02-09 07:07:19 +0900

    Initial Commit

---
+++ YouTubeAPI/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  YouTubeAPI
+//
+//  Created by Miyoshi Masataka on 2018/02/09.
+//  Copyright © 2018年 Masataka Miyoshi. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
