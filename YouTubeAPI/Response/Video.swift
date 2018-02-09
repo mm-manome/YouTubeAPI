748f8c44e538427e4aafeba473e3305e6983cd8c
Masataka Miyoshi <masataka.ehime.univ@gmail.com>
2018-02-09 15:08:23 +0900

    Decodableに準拠したモデルを生成

---
+++ YouTubeAPI/Response/Video.swift
@@ -0,0 +1,21 @@
+//
+//  Video.swift
+//  YouTubeAPI
+//
+//  Created by Miyoshi Masataka on 2018/02/09.
+//  Copyright © 2018年 Masataka Miyoshi. All rights reserved.
+//
+
+import Foundation
+
+struct Video: Decodable {
+    
+    // MARK: Properties
+    
+    let etag: String
+    
+    let kind: String
+    let snippet: Snippet
+    
+    
+}
